e985e6b6fb24ac4b48b431f2e73ed5ef79d7e1ff
Молчанов <ultimate.rus.e@gmail.com>
2026-08-26 22:01:15 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,18 +19,26 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// ПРАВИЛЬНЫЙ ФИКС ВЕРСИЙ ДЛЯ GRADLE 9+:
+// ГЛОБАЛЬНЫЙ И БЕЗОТКАЗНЫЙ ФИКС ДЛЯ ВСЕХ ПЛАГИНОВ (ВКЛЮЧАЯ TELEPHONY):
 subprojects {
     plugins.withId("com.android.application") {
         configure<com.android.build.gradle.BaseExtension> {
             compileSdkVersion(34)
             defaultConfig { targetSdk = 34 }
+            // Если у плагина нет namespace, даем ему имя на основе его названия
+            if (namespace == null) {
+                namespace = "com.example.${project.name}"
+            }
         }
     }
     plugins.withId("com.android.library") {
         configure<com.android.build.gradle.BaseExtension> {
             compileSdkVersion(34)
             defaultConfig { targetSdk = 34 }
+            // Фикс ошибки Namespace not specified для старых библиотек
+            if (namespace == null) {
+                namespace = "com.example.${project.name}"
+            }
         }
     }
 }
