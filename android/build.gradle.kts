765444261ee90992f0d3f776759d027abde23d83
Молчанов <ultimate.rus.e@gmail.com>
2026-08-26 22:09:49 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,13 +19,12 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// ГЛОБАЛЬНЫЙ И БЕЗОТКАЗНЫЙ ФИКС ДЛЯ ВСЕХ ПЛАГИНОВ (ВКЛЮЧАЯ TELEPHONY):
+// ВЫСТАВИЛИ АКТУАЛЬНУЮ ВЕРСИЮ 35 ДЛЯ ВСЕХ ПЛАГИНОВ:
 subprojects {
     plugins.withId("com.android.application") {
         configure<com.android.build.gradle.BaseExtension> {
-            compileSdkVersion(34)
-            defaultConfig { targetSdk = 34 }
-            // Если у плагина нет namespace, даем ему имя на основе его названия
+            compileSdkVersion(35)
+            defaultConfig { targetSdk = 35 }
             if (namespace == null) {
                 namespace = "com.example.${project.name}"
             }
@@ -33,9 +32,8 @@ subprojects {
     }
     plugins.withId("com.android.library") {
         configure<com.android.build.gradle.BaseExtension> {
-            compileSdkVersion(34)
-            defaultConfig { targetSdk = 34 }
-            // Фикс ошибки Namespace not specified для старых библиотек
+            compileSdkVersion(35)
+            defaultConfig { targetSdk = 35 }
             if (namespace == null) {
                 namespace = "com.example.${project.name}"
             }
