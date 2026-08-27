7ada3320a68339ed4ffb5e049a249da4b280f332
Молчанов <ultimate.rus.e@gmail.com>
2026-08-28 01:40:51 +0300

    Update targetSdk version from 35 to 34

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         applicationId = "com.example.untitled"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         multiDexEnabled = true
