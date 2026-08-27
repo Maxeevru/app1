affb74ba6c9e870938cc77fde5d1e8526df1c72f
Молчанов <ultimate.rus.e@gmail.com>
2026-08-27 22:50:13 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,52 +1,18 @@
-import java.util.Properties
-
-val localProperties = Properties().apply {
-    val localPropertiesFile = rootProject.file("local.properties")
-    if (localPropertiesFile.exists()) {
-        localPropertiesFile.inputStream().use { inputStream ->
-            load(inputStream)
-        }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-val flutterRoot = localProperties.getProperty("flutter.sdk")
-    ?: throw GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file.")
-
-val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toIntOrNull() ?: 1
-val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
-
-apply(plugin = "com.android.application")
-apply(plugin = "kotlin-android")
-apply(from = "$flutterRoot/packages/flutter_tools/gradle/flutter.gradle")
-
-android {
-    compileSdk = 35
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    defaultConfig {
-        applicationId = "com.example.emergency_alert"
-        minSdk = 21
-        targetSdk = 35
-        versionCode = flutterVersionCode
-        versionName = flutterVersionName
-        multiDexEnabled = true
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
+rootProject.buildDir = "../build"
+subprojects {
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
-flutter {
-    source = "../.."
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
