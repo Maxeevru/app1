583cef8ac8c4ceb7f0f2436f01417cb74585538d
Молчанов <ultimate.rus.e@gmail.com>
2026-08-28 01:11:29 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.untitled
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import com.shounakmulay.telephony.TelephonyPlugin
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // Инициализация телефонии, чтобы релизная сборка не падала
+        TelephonyPlugin.registerWith(flutterEngine)
+    }
+}
